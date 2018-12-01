7a84ad255a260f7abf7c8aa068e950a8a2c23b60
Mohamed Mounir <m.mounir.f@gmail.com>
2018-12-01 18:41:55 +0200

    Updates

---
+++ precache-manifest.99f16fbbfa46f40adca14ee81774a319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a1e2e5f652fffcf8ee2",
+    "url": "/chat-bot/static/js/runtime~main.0a1e2e5f.js"
+  },
+  {
+    "revision": "5424fee860555114f919",
+    "url": "/chat-bot/static/js/main.5424fee8.chunk.js"
+  },
+  {
+    "revision": "2effe6f11475b38a7236",
+    "url": "/chat-bot/static/js/1.2effe6f1.chunk.js"
+  },
+  {
+    "revision": "5424fee860555114f919",
+    "url": "/chat-bot/static/css/main.87148123.chunk.css"
+  },
+  {
+    "revision": "15cf5d60c4267c93c3055f803b252ad7",
+    "url": "/chat-bot/index.html"
+  }
+];
